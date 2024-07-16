bdafea20f8784c4afd23ef033cf85d0b7f16452b
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-07-16 14:09:07 +0300

    Initial Commit

---
+++ src/main/java/com/archisacademy/career_hive/CareerHiveApplication.java
@@ -0,0 +1,13 @@
+package com.archisacademy.career_hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerHiveApplication.class, args);
+	}
+
+}
