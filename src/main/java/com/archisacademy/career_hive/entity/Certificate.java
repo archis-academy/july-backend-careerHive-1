e9feee6d44bf9802464f345883d57df506f113c2
Emre Aybar <aybaremre5@gmail.com>
2024-07-19 19:22:54 +0300

    [JU-21] feature and certificate classes have been added.

---
+++ src/main/java/com/archisacademy/career_hive/entity/Certificate.java
@@ -0,0 +1,44 @@
+package com.archisacademy.career_hive.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.print.attribute.standard.Media;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "certificates")
+public class Certificate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title_name",nullable = false)
+    private String titleName;
+
+    @Column(name = "provided_name",nullable = false)
+    private String providedName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "role",nullable = false,length = 40)
+    private String role;
+
+    @Column(name = "date")
+    private Timestamp date;
+
+    @Column(name = "skill",nullable = false)
+    private String skill;
+
+    @Column(name = "user_id")
+    private long userId;
+    
+}
