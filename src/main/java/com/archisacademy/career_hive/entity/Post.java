3b9031559a71b343ca11e836921f0385bff5a026
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-07-24 12:33:25 +0300

    [JU-06]-like-entity-repository

---
+++ src/main/java/com/archisacademy/career_hive/entity/Post.java
@@ -0,0 +1,11 @@
+package com.archisacademy.career_hive.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    private Long id;
+}
